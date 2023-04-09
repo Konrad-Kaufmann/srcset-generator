8b999472717b75593065af46233b7db3d142e718
Konrad-Kaufmann <kaufmann.konrad05@gmail.com>
2023-04-09 22:45:27 +0200

    pls do your work big gods of github

--- src/main/java/com/example/MainClass.java
+++ src/main/java/com/example/MainClass.java
@@ -9,9 +9,6 @@ public class MainClass {
 
     public static void main(String[] args) throws InterruptedException {
         // Application.launch(UIMainFX.class, args);
-
         new MainApp();
-
     }
-
 }
